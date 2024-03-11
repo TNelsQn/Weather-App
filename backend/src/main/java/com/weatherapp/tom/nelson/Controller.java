7e66543235b93c6147f5a7b0c091fc1ed3c4a9b1
TNelsQn <81658190+TNelsQn@users.noreply.github.com>
2024-03-11 23:07:07 +0000

    Update Controller.java

--- backend/src/main/java/com/weatherapp/tom/nelson/Controller.java
+++ backend/src/main/java/com/weatherapp/tom/nelson/Controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RestController
 public class Controller {
 
-    final String uri = ["REPLAC WITH YOUR OWN API KEY.  EASILY GET ONE FROM OPENWEATHER.COM"];
+    final String uri = ["REPLACE WITH YOUR OWN API KEY.  EASILY GET ONE FROM OPENWEATHER.COM"];
     final String api = "0e1131d928da1154f0c342376fd23dee";
 
     @GetMapping(path = "/weather")

a45f030f3e73b15cc43edd3f11ecba80a62ff527
TNelsQn <81658190+TNelsQn@users.noreply.github.com>
2024-03-13 17:22:06 +0000

    finished project

--- backend/src/main/java/com/weatherapp/tom/nelson/Controller.java
+++ backend/src/main/java/com/weatherapp/tom/nelson/Controller.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RestController
 public class Controller {
 
-    final String uri = ["REPLACE WITH YOUR OWN API KEY.  EASILY GET ONE FROM OPENWEATHER.COM"];
-    final String api = "0e1131d928da1154f0c342376fd23dee";
+    final String uri = "http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+    final String api = "[REPLACE STRING WITH YOUR API KEY FROM OPENWEATHER.ORG]";
 
     @GetMapping(path = "/weather")
     public String findWeather(@RequestParam String city) {
